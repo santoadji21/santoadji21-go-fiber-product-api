389be70a926bdff71c685ba8082ee410941871dd
Ahmad Aji Santoso <santoadji21@emp.blankontech.com>
2023-12-17 15:10:45 +0700

    fix: snake_case response

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,6 +6,6 @@ import (
 
 type Model struct {
 	ID        uint      `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
